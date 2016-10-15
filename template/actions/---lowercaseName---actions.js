322d8c4ec1fa2362b3c1fc44c14e4ae5deaed56b
Alaa El Helw <work@sunny.fr>
2016-10-15 16:27:12 +0200

    fix vars

--- template/actions/---lowercaseName---actions.js
+++ template/actions/---lowercaseName---actions.js
@@ -10,12 +10,12 @@ const url_list = config.paths.streams.list
 **/
 export function fetch<%= capitalizeName %> (params) {
   return function (dispatch) {
-    dispatch({type: "FETCH_<%= uppcaseName %>", payload: {params}})
+    dispatch({type: "FETCH_<%= uppercaseName %>", payload: {params}})
     const url = url_item
     axios.get(url).then(response=>{
-      dispatch({type: "FETCH_<%= uppcaseName %>_FULFILLED", payload: {data: response.data, params}})
+      dispatch({type: "FETCH_<%= uppercaseName %>_FULFILLED", payload: {data: response.data, params}})
     }).catch((error) => {
-      dispatch({type: "FETCH_<%= uppcaseName %>_REJECTED", payload: {error, params}})
+      dispatch({type: "FETCH_<%= uppercaseName %>_REJECTED", payload: {error, params}})
     })
   }
 }
@@ -26,13 +26,13 @@ export function fetch<%= capitalizeName %> (params) {
 export function fetch<%= capitalizeName %>s (type) {
   return function (dispatch) {
     const url = url_list.replace('{type}', type)
-    dispatch({type: 'FETCH_<%= uppcaseName %>S', payload: {type}})
+    dispatch({type: 'FETCH_<%= uppercaseName %>S', payload: {type}})
     axios.get(url)
       .then((response) => {
-        dispatch({type: 'FETCH_<%= uppcaseName %>S_FULFILLED', payload: {data: response.data, type}})
+        dispatch({type: 'FETCH_<%= uppercaseName %>S_FULFILLED', payload: {data: response.data, type}})
       })
       .catch((error) => {
-        dispatch({type: 'FETCH_<%= uppcaseName %>S_REJECTED', payload: {error, type}})
+        dispatch({type: 'FETCH_<%= uppercaseName %>S_REJECTED', payload: {error, type}})
       })
   }
 }
